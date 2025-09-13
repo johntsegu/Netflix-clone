93abe56a96374a6fbaf4573b352ff640fb997622
johntsegu <jonitsegu12@gmail.com>
2025-09-13 14:37:21 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "movie-app-clone-2025",
+  base: "/movie-app-clone-2025/",
   plugins: [react()],
 })
